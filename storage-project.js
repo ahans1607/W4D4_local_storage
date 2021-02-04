6d91cc3621fa7ebb25f22c206356931ce8c7714b
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-04 14:53:29 -0600

    3

--- storage-project.js
+++ storage-project.js
@@ -32,12 +32,28 @@ let storeItems = function(event){
   let myQty = document.getElementById("quantity")
 
   localStorage.setItem(myItem.value, myQty.value);
+  showCart();
 }
 
-let showCart = function (event){
-    let myItem = document.getElementById("items");
-    let myQty = document.getElementById("quantity")
-    console.log(localStorage.getItem(myItem.value));
+{/* <p>testItem</p> */}
 
+let showCart = function (){
+    // let myItem = document.getElementById("items");
+    // let myQty = document.getElementById("quantity")
+    // console.log(localStorage.getItem(myItem.value));
+  let keys = Object.keys(localStorage);
+  let values = Object.values(localStorage);
+  // console.log(values)
+  // console.log(keys);
+  //   for (let i = 0; i < keys.length; i++){ 
+  //     console.log(localStorage.getItem(keys[i]))
 
+  //   }
+  for (let i = 0; i < keys.length; i++){
+    let myNewItem = document.createElement("p")
+     myNewItem.innerText = keys[i] + " " + values[i];
+      myNewItem.id = keys[i]
+        document.getElementById('shopping-cart').append(myNewItem);
+
+  }
 }
